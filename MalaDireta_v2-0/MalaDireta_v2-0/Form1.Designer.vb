cc7552c8ea6c3fa7da824bc87af6d4c23094278d
Fernando Santos <fgsantos@wllm.com.br>
2014-06-10 12:39:19 -0300

    -Web Service "WSSINALIZA" adicionado; -Tela de consulta para o caso específico do Sr. José Carlos adicionada; -tela de configuração adicionada

--- MalaDireta_v2-0/MalaDireta_v2-0/Form1.Designer.vb
+++ MalaDireta_v2-0/MalaDireta_v2-0/Form1.Designer.vb
@@ -22,9 +22,30 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.SuspendLayout()
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(1127, 12)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(85, 21)
+        Me.Button1.TabIndex = 2
+        Me.Button1.Text = "Configuração"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.Text = "Form1"
+        Me.ClientSize = New System.Drawing.Size(1224, 691)
+        Me.Controls.Add(Me.Button1)
+        Me.Name = "Form1"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "but"
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents Button1 As System.Windows.Forms.Button
 
 End Class
